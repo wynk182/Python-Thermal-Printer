eca0e4d680d6fbd6cbbc68dadaf2de5f5cc84ece
Michael <wynk182@gmail.com>
2018-10-14 10:12:05 -0700

    updated print queue for testing

--- Print_Queue.py
+++ Print_Queue.py
@@ -1,6 +1,9 @@
 import json
 import qrcode
 import urllib2
+from Adafruit_Thermal import *
+
+printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
 class Print_Queue():
 
@@ -13,21 +16,25 @@ class Print_Queue():
     def print_ticket():
         print('ticket printing')
 
-    url = "http://localhost:3000/printer/auth.json"
+    url = "http://mossbee.ngrok.io/printer/auth.json"
     auth_header = '{"email": "michael@mossbee.com", "password":"123456ADMIN"}'
     # print(contents)
     json_data = send_request(auth_header, url)
     print(json_data)
 
     access_token = json_data['access_token']
-    url = "http://localhost:3000/printer/get_queue/1.json"
+    url = "http://mossbee.ngrok.io/printer/get_queue/1.json"
     auth_header = '{"access_token": "'+access_token+'"}'
     orders = send_request(auth_header, url)
     # print orders
     for val in orders:
-        print(val['order_items'])
-        print(val['name'])
-        qrcode.make("https://mossbee.ngrok.io/menus/1/orders/" + str(val['id'])).save(str(val['order_number']) + '.bmp')
+        printer.justify('L')
+        printer.println(val['name'])
+        printer.println(val['order_items'])
+        #print(val['order_items'])
+        #print(val['name'])
+        printer.printBitmap(50, 50, qrcode.make("https://mossbee.ngrok.io/menus/1/orders/" + str(val['id'])))
+        #printer.qrcode.make("https://mossbee.ngrok.io/menus/1/orders/" + str(val['id']))#.save(str(val['order_number']) + '.bmp')
     # for val in json_data:
     #     qrcode.make(val['qr_url']).save(val['name'] + '.png')
 
